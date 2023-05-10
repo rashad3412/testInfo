e43e2345fdd364a33a5403b519ff390fee8e5fe6
rashad3412 <rashd3412@gmail.com>
2023-05-10 15:44:57 -0400

    Block18

--- testing.js
+++ testing.js
@@ -14,22 +14,22 @@
 //** Should take two arrays of integers as arguments */
 //** Should return a single array that contains odd numbers in asceding order from both arrays */
 
-const concatOdds = () => {
-    let a = [3, 2, 1]
-    let b = [9, 1, 1, 1, 4, 15, -1]
-    let c = []
-    const arr = a.concat(b)
-    for (let i = 0; i <= arr.length; i++){
+const concatOdds = () => { // Using arrow function method
+    let a = [3, 2, 1] // Using array for value of A
+    let b = [9, 1, 1, 1, 4, 15, -1] // using array for value of B
+    let c = [] // Creating empty array to store odd number values
+    const arr = a.concat(b) // using concat method to combine both a and b values
+    for (let i = 0; i <= arr.length; i++){ // Using for loop to iterate over a and b values to get odd numbers.
 
-        if (arr[i] % 2 === 1){
-            c.push(arr[i])
+        if (arr[i] % 2 === 1){ // using if statement to access each index in both a and b that is store in the arr varible 
+            c.push(arr[i]) // pushing each odd number from arr varible into new array called C.
 
-        }else if(arr[i] % 2 === 0){
-            console.log("numbers are even", arr[i])
+        }else if(arr[i] % 2 === 0){ // Using if statement to track the even numbers in the arr varible.
+            console.log("numbers are even", arr[i]) // logging even numbers from the arr varible. 
         }
 
-    }console.log(c.sort())
+    }console.log(c.sort()) // logging the numbers that is stored in the new c array.
 
 }
-(concatOdds())
+(concatOdds()) // expected output should be odd numbers stored in an array and even numbers printed out with string "numbers are even" and the nunber that is even.
 
